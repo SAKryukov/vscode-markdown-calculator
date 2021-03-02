911401843d69b45e38d713fe1432739dafb2ebdc
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2021-03-02 03:20:54 -0500

    Fully working skeleton

--- docs/test.md
+++ docs/test.md
@@ -1,33 +1,10 @@
-@calculate{
+## Heading 1
+
+``` @calculate
    const x = 1;
    const y = 12;
    console.log(x + y);
+   return x + y + "%";
+```
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+### Heading 1.1
--- extension/calculator.js
+++ extension/calculator.js
@@ -2,27 +2,16 @@
 
 module.exports = (md, options) => {
 
-    const functionMap = new Map();
-    const functionRegexp = new RegExp(/\@calculate\{(.*)\}/, "sgm");
-
     const renderFunction = body => {
-        return("<p>calc result");
+        let value = undefined;
+        try {
+            const f = Function("", body);
+            value = f();
+        } catch (ex) {
+            return(`<p style="color:red">${ex.toString()}</p>`);
+        }
+        return(`<p>Result: ${value}</p>`);
     }; //
-
-    md.core.ruler.after("block", "collectFunctions", state => {
-        for (let index = 0; index < state.tokens.length; ++index) {
-            const token = state.tokens[index];
-            const isParagraph = token.type == "paragraph_open";
-            if (!isParagraph) continue;
-            const contentToken = state.tokens[index + 1];
-            if (!contentToken) continue;
-            const match = functionRegexp.exec(contentToken.content);
-            if (match) {
-                const body = match[1];
-                if (body) functionMap.set(index, body);
-            } //if
-        } //loop tokens
-    }); //md.core.ruler.after
 
     const renderDefault = (tokens, index, options, object, renderer, previousHandler, defaultHtml) => {
         if (previousHandler)
@@ -31,30 +20,12 @@ module.exports = (md, options) => {
             return defaultHtml;
     }; //renderDefault
 
-    let ignoringText = false;
-
-    const previousRenderParagraphOpen = md.renderer.rules.paragraph_open;
-    md.renderer.rules.paragraph_open = (tokens, index, ruleOptions, object, renderer) => {
-        const body = functionMap.get(index);
-        if (!body)
-            return renderDefault(tokens, index, ruleOptions, object, renderer, previousRenderParagraphOpen, `<p>`);
-        ignoringText = true;
-        return renderFunction(body);
+    const previousRender = md.renderer.rules.fence;
+    md.renderer.rules.fence = (tokens, index, ruleOptions, object, renderer) => {
+        if (tokens[index].info.trim() == "@calculate")
+            return `${renderFunction(tokens[index].content)}`;
+        else
+            return renderDefault(tokens, index, ruleOptions, object, renderer, previousRender);
     }; //md.renderer.paragraph_open
-
-    const previousRenderParagraphClose = md.renderer.rules.paragraph_close;
-    md.renderer.rules.paragraph_close = (tokens, index, ruleOptions, object, renderer) => {
-        ignoringText = false;
-        return renderDefault(tokens, index, ruleOptions, object, renderer, previousRenderParagraphClose);
-    }; //md.renderer.paragraph_open
-
-
-    const previousRender = md.renderer.rules.text;
-    md.renderer.rules.text = (tokens, index, ruleOptions, object, renderer) => {
-        if (ignoringText)
-            return "";
-        return renderDefault(tokens, index, ruleOptions, object, renderer, previousRender);
-    }; //md.renderer.paragraph_open
-
 
 }; //module.exports
